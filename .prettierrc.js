806f76266f7e46c68cc7f6677e394feb6ea727bd
Hydrogen <3038094028@qq.com>
2022-08-03 10:57:46 +0800

    chore: setup

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  printWidth: 80,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  htmlWhitespaceSensitivity: 'strict',
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'always',
+}
